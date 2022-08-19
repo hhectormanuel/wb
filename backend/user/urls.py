f9ab086abdd4958c6614c07c3bd62866d36e028b
Hector <hectorcena@live.com.mx>
2022-08-19 12:06:58 -0600

    fallas en el user

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls.static import static
+from django.urls import path
 from django.conf import settings
+from .viewset import pruebaView
+from .views import UserApiView
+
+app_name='user'
 
 urlpatterns = [
+    path('profile/<slug:slug>', UserApiView, name='profile')
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializer import UserSerializer, UserExtendSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import UserExtend
 
-# Create your views here.
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+
+
+@api_view(['GET', 'POST'])
+def UserApiView(request, slug):
+    if request.method == 'GET':
+        userExtend = UserExtend.objects.get(slug = slug)
+        user = userExtend.user
+        userSerializer = UserExtendSerializer(userExtend)
+        return Response(userSerializer.data, status=status.HTTP_200_OK)
+
